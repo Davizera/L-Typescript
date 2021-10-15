19caf7720ddb488b0c2d9029f520fa6b2ab93ac7
Davizera <davi.sousa10@gmail.com>
2021-10-15 15:41:05 -0300

    ajeitando arquitetura da aplicação

---
+++ app/dist/js/views/app.js
@@ -0,0 +1,12 @@
+import { NegociacaoController } from "../controllers/negociacao-controller.js";
+const form = document.querySelector(".form");
+const controller = new NegociacaoController();
+if (form) {
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        controller.adiciona();
+    });
+}
+else {
+    throw Error("Não foi possível iniciar a aplicação!");
+}
